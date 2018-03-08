46cd7ec35bb2ee17c87eb4d3e8c93850fb54d3f2
hoangtrieukd <hoangtrieukd@gmail.com>
2018-03-08 16:17:36 +0700

    fix mmiss permit param

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -62,6 +62,7 @@ class PostsController < ApplicationController
   end
 
   private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
@@ -69,6 +70,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content, :category_id)
+      params.require(:post).permit(:title, :content, :category_id, :image)
     end
 end
