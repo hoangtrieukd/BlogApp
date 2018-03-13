f5e2dd6d81f8701ecab4d02aa1ee0859cff5f976
hoangtrieukd <hoangtrieukd@gmail.com>
2018-03-13 19:21:38 +0700

    add rbenv_path

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :use_sudo, true
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.3.1'
-# set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_path, '/home/deploy/.rbenv'
 set :rbenv_map_bins, %w[rake gem bundle ruby rails]
 set :rbenv_roles, :all # default value
 # Default value for default_env is {}
